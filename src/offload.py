3902c5ef79e80fb06fef6b611beec31842edbbce
bobolle <botdahlstrom@gmail.com>
2025-03-28 22:45:37 +0100

    add try finally statement to exit offload

--- src/offload.py
+++ src/offload.py
@@ -1,11 +1,17 @@
 import time
 
-def sse(env, sr):
+def stream(env, sr):
     headers = []
     headers.append(('Content-Type','text/event-stream'))
     headers.append(('Cache-Control','no-cache'))
+    headers.append(('Connection','keep-alive'))
     sr('200 OK', headers)
 
-    while True:
-        time.sleep(1)
-        yield b'data: [{"device_id": "pico_w", "data": 30}]\n\n'
+    # trying to yield to closed socket will make uwsgi throw an OSError: write error
+    # nothing to worry about if we exit from this instance
+    try:
+        while True:
+            yield b'data: [{"device_id": "pico_w", "data": 30}]\n\n'
+            time.sleep(1)
+    finally:
+        pass
